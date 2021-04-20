d4760a5e30268ee91e4e5d3f089bd276eb820a8f
romyabbrederis <romy.abbrederis@gmail.com>
2021-04-20 20:45:45 +0200

    changed meta image tags

--- pages/[...pid].js
+++ pages/[...pid].js
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import React, { useEffect, useRef, useState } from "react";
 import Layout from "../components/layout";
 import {
@@ -11,8 +11,8 @@ import {
   Box22,
   Box33,
   Text,
-  ImageContainer
-} from '../styles/styles';
+  ImageContainer,
+} from "../styles/styles";
 import aboutText from "../data/about.json";
 import apparelText from "../data/apparel.json";
 import cmtText from "../data/cmt.json";
@@ -21,46 +21,48 @@ import fppText from "../data/fpp.json";
 import patternText from "../data/pattern-making.json";
 import photographyText from "../data/photography.json";
 import shippingText from "../data/shipping.json";
-import Image from 'next/image';
-import Head from 'next/head'
+import Image from "next/image";
+import Head from "next/head";
 
 const Pages = () => {
   const [currentPath, setCurrentPath] = useState();
   const pageRef = useRef(null);
-  const router = useRouter()
-  const { pid } = router.query
+  const router = useRouter();
+  const { pid } = router.query;
 
   const Line = (
     <svg
-      width='60'
-      height='10'
-      xmlns='http://www.w3.org/2000/svg'
-      version='1.1'
-      className='scale-in-left mt-4'
+      width="60"
+      height="10"
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      className="scale-in-left mt-4"
     >
       <line
-        x1='40'
-        x2='5'
-        y1='5'
-        y2='5'
-        stroke='#CEC6B6'
-        strokeWidth='2'
-        strokeLinecap='butt'
+        x1="40"
+        x2="5"
+        y1="5"
+        y2="5"
+        stroke="#CEC6B6"
+        strokeWidth="2"
+        strokeLinecap="butt"
       />
     </svg>
-  )
+  );
 
   const data = [
-
     {
       index: 1,
       url: "cmt",
       image: "/images/cmt.jpg",
       headline: cmtText.headline.toUpperCase(),
       subheadline: cmtText.subheadline.toUpperCase(),
-      text: [cmtText.paragraph1.toUpperCase(), cmtText.paragraph2.toUpperCase()],
+      text: [
+        cmtText.paragraph1.toUpperCase(),
+        cmtText.paragraph2.toUpperCase(),
+      ],
       nextUrl: "manufacturing-fpp",
-      nextName: "FPP"
+      nextName: "FPP",
     },
     {
       index: 2,
@@ -68,9 +70,12 @@ const Pages = () => {
       image: "/images/fpp.jpg",
       headline: fppText.headline.toUpperCase(),
       subheadline: fppText.subheadline.toUpperCase(),
-      text: [fppText.subheadline.toUpperCase(), fppText.paragraph2.toUpperCase()],
+      text: [
+        fppText.subheadline.toUpperCase(),
+        fppText.paragraph2.toUpperCase(),
+      ],
       nextUrl: "pattern-making",
-      nextName: "PATTERN MAKING"
+      nextName: "PATTERN MAKING",
     },
 
     {
@@ -82,10 +87,10 @@ const Pages = () => {
       text: [
         patternText.paragraph1.toUpperCase(),
         patternText.paragraph2.toUpperCase(),
-        patternText.paragraph3.toUpperCase()
+        patternText.paragraph3.toUpperCase(),
       ],
       nextUrl: "fabric-trim-sourcing",
-      nextName: "FABRIC TRIM SOURCING"
+      nextName: "FABRIC TRIM SOURCING",
     },
     {
       index: 4,
@@ -95,10 +100,10 @@ const Pages = () => {
       // subheadline: fabricTrimText.subheadline.toUpperCase(),
       text: [
         fabricTrimText.paragraph1.toUpperCase(),
-        fabricTrimText.paragraph2.toUpperCase()
+        fabricTrimText.paragraph2.toUpperCase(),
       ],
       nextUrl: "apparel-production",
-      nextName: "APPAREL PRODUCTION"
+      nextName: "APPAREL PRODUCTION",
     },
     {
       index: 5,
@@ -109,10 +114,10 @@ const Pages = () => {
       text: [
         apparelText.paragraph1.toUpperCase(),
         apparelText.paragraph2.toUpperCase(),
-        apparelText.paragraph3.toUpperCase()
+        apparelText.paragraph3.toUpperCase(),
       ],
       nextUrl: "photography",
-      nextName: "PHOTOGRAPHY"
+      nextName: "PHOTOGRAPHY",
     },
     {
       index: 6,
@@ -122,10 +127,10 @@ const Pages = () => {
       subheadline: photographyText.subheadline.toUpperCase(),
       text: [
         photographyText.paragraph1.toUpperCase(),
-        photographyText.paragraph2.toUpperCase()
+        photographyText.paragraph2.toUpperCase(),
       ],
       nextUrl: "shipping",
-      nextName: "SHIPPING"
+      nextName: "SHIPPING",
     },
     {
       index: 7,
@@ -133,9 +138,12 @@ const Pages = () => {
       image: "/images/ship.jpg",
       headline: shippingText.headline.toUpperCase(),
       subheadline: shippingText.subheadline.toUpperCase(),
-      text: [shippingText.paragraph1.toUpperCase(), shippingText.paragraph2.toUpperCase()],
+      text: [
+        shippingText.paragraph1.toUpperCase(),
+        shippingText.paragraph2.toUpperCase(),
+      ],
       nextUrl: "about",
-      nextName: "ABOUT"
+      nextName: "ABOUT",
     },
     {
       index: 8,
@@ -143,9 +151,12 @@ const Pages = () => {
       image: "/images/about.jpg",
       headline: aboutText.headline.toUpperCase(),
       subheadline: aboutText.subheadline.toUpperCase(),
-      text: [aboutText.paragraph1.toUpperCase(), aboutText.paragraph2.toUpperCase()],
+      text: [
+        aboutText.paragraph1.toUpperCase(),
+        aboutText.paragraph2.toUpperCase(),
+      ],
       nextUrl: "faq",
-      nextName: "FAQ"
+      nextName: "FAQ",
     },
   ];
 
@@ -160,30 +171,40 @@ const Pages = () => {
     setCurrentPath(path[1]);
   }, [pid]);
 
-  return data.map(item => {
+  return data.map((item) => {
     if (currentPath === item.url) {
       return (
         <div key={Date.now()}>
           <Head>
             <title>{item.headline} | Faktory Studios | Serbia Europe</title>
-            <link rel='icon' type='image/png' href='/faktory.png' sizes='32x32' />
-            <meta
-              name='description'
-              content={item.text[0] + ', ' + item.text[1]}
+            <link
+              rel="icon"
+              type="image/png"
+              href="/faktory.png"
+              sizes="32x32"
             />
-            <meta name='viewport' content='width=device-width, initial-scale=1.0' />
             <meta
-              property='og:title'
+              name="description"
+              content={item.text[0] + ", " + item.text[1]}
+            />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+            <meta
+              property="og:title"
               content={`FAKTORY STUDIOS | Clothing Manufacturing in Serbia Europe | CMT Serbia | FPP Serbia | Clothing Apparel producer in Serbia Europe | clothing manufacturing serbia | garment manufacturing serbia | apparel production serbia | pattern making serbia | fpp serbia | cmt serbia | fashion brand production serbia | cheap production europe | `}
             />
-            <meta property='og:description' content='FAKTORY STUDIOS | Clothing apparel manufacturing in Serbia Europe' />
-            <meta property='og:image' content='https://i.ibb.co/M5gspLj/FKT-Studios.png' />
-            <meta property='og:image:secure_url' content='https://i.ibb.co/M5gspLj/FKT-Studios.png' />
-            <meta property='og:image:type' content='image/jpeg' />
-            <meta property='og:image:width' content='640' />
-            <meta property='og:image:height' content='442' />
-            <meta property='og:url' content='http://faktory-studios.com/' />
-
+            <meta
+              property="og:description"
+              content="FAKTORY STUDIOS | Clothing apparel manufacturing in Serbia Europe"
+            />
+            <meta property="og:image" content={item.image} />
+            <meta property="og:image:secure_url" content={item.image} />
+            <meta property="og:image:type" content="image/jpeg" />
+            <meta property="og:image:width" content="1200" />
+            <meta property="og:image:height" content="600" />
+            <meta property="og:url" content="http://faktory-studios.com/" />
           </Head>
 
           <Layout ref={pageRef}>
@@ -193,7 +214,7 @@ const Pages = () => {
                 <Headline>{item.headline}</Headline>
               </Box1>
               <Box22>
-                {item.text.map(el => (
+                {item.text.map((el) => (
                   <>
                     <Text>{el}</Text>
                     <p></p>
--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,36 @@
-import '../styles/globals.css'
-import { globalStyles } from '../styles/styles'
-import Head from 'next/head'
+import "../styles/globals.css";
+import { globalStyles } from "../styles/styles";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }) => (
   <>
     <Head>
       <title>FAKTORY STUDIOS</title>
-      <link rel='icon' type='image/png' href='/faktory.png' sizes='32x32' />
+      <link rel="icon" type="image/png" href="/faktory.png" sizes="32x32" />
+      <link rel="shortcut icon" href="../favicon.ico" />
       <meta
-        name='description'
-        content={'FAKTORY STUDIOS | Manufacturing CMT FPP clothing apparel producer in Serbia Europe '}
+        name="description"
+        content={
+          "FAKTORY STUDIOS | Manufacturing CMT FPP clothing apparel producer in Serbia Europe "
+        }
       />
-      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
-        property='og:title'
+        property="og:title"
         content={`FAKTORY STUDIOS | Clothing Manufacturing in Serbia Europe | CMT Serbia | FPP Serbia | Clothing Apparel producer in Serbia Europe | clothing manufacturing serbia | garment manufacturing serbia | apparel production serbia | pattern making serbia | fpp serbia | cmt serbia | fashion brand production serbia | cheap production europe | `}
       />
-      <meta property='og:description' content='FAKTORY STUDIOS | Clothing apparel manufacturing in Serbia Europe' />
-      <meta property='og:image' content='https://i.ibb.co/M5gspLj/FKT-Studios.png' />
-      <meta property='og:image:secure_url' content='https://i.ibb.co/M5gspLj/FKT-Studios.png' />
-      <meta property='og:image:type' content='image/jpeg' />
-      <meta property='og:image:width' content='640' />
-      <meta property='og:image:height' content='442' />
-      <meta property='og:url' content='http://faktory-studios.com/' />
+      <meta
+        property="og:description"
+        content="FAKTORY STUDIOS | Clothing apparel manufacturing in Serbia Europe"
+      />
+      <meta property="og:image" content="../logo.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="600" />
+      <meta property="og:url" content="http://faktory-studios.com/" />
     </Head>
     {globalStyles}
     <Component {...pageProps} />
   </>
-)
+);
 
-export default App
+export default App;
